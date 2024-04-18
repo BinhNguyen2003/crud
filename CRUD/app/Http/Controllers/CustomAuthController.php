86fc7c4c1a8884d91153b4b7354e4045bf373ad8
BinhNguyen2003 <binhcrbay07@gmail.com>
2024-04-18 21:35:07 +0700

    update

--- CRUD/app/Http/Controllers/CustomAuthController.php
+++ CRUD/app/Http/Controllers/CustomAuthController.php
@@ -22,6 +22,82 @@ public function show()
         $user = User::all();
         return view('auth.viewuser', compact('user'));
     } 
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function customLogin(Request $request)
+    {
+     
+        $email = $request->get('email');
+        $data = $request->all();
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard')
+                ->withSuccess('Signed in');
+        }
+
+        return redirect("login")->withSuccess('Login details are not valid');
+    }
+
+    public function registration()
+    {
+        return view('auth.registration');
+    }
+
+    public function customRegistration(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+           'email' => 'required|email|unique:users',
+           'password' => 'required|min:6',
+        ]);
+
+        $data = $request->all();
+        //upload
+        $file = $request->file('fileToUpload');
+        $fileName = $file->getClientOriginalName();
+        $destinationPath = 'uploads';
+        $file->move($destinationPath, $file->getClientOriginalName());
+
+        $data['fileName'] = $fileName;
+        $check = $this->create($data);
+
+        return redirect("login")->withSuccess('You have signed-in');
+    }
+
+    public function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'phone' => $data['phone'],
+            'image' => $data['fileName'],
+            'password' => Hash::make($data['password'])
+        ]);
+    }
+
+    public function dashboard()
+    {      
+        //View
+        if (Auth::check()) {
+            return view('auth.viewuser');
+        }            
+        return redirect("login")->withSuccess('You are not allowed to access');
+    }
+    public function signOut()
+    {
+        //Logout 
+        Session::flush();
+        Auth::logout();
+        return Redirect('login');
+    }
 }
 
 
